5c77186d2e718a53370cca1d549404f621e97c45
Denis Chumakov <chumakov@azoft.com>
2021-03-19 22:43:51 +0700

    'deploy'

---
+++ precache-manifest.b831079128168645ca659879d4413ba5.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a071608911285d262c72",
+    "url": "/css/app.c2d7b832.css"
+  },
+  {
+    "revision": "d727346ef53102bee957",
+    "url": "/css/chunk-vendors.5f530648.css"
+  },
+  {
+    "revision": "e35ea6454653a10a52c4",
+    "url": "/css/footer.3ed0440f.css"
+  },
+  {
+    "revision": "0b1de269038a848e7995",
+    "url": "/css/footer~login.378368e7.css"
+  },
+  {
+    "revision": "e434cc44b6c541eb0d64",
+    "url": "/css/login.a4ee2251.css"
+  },
+  {
+    "revision": "82c44e34e816ff3f80f35420adc778d2",
+    "url": "/img/avatars/logo.jpg"
+  },
+  {
+    "revision": "6b66933beeb6b0cfc7807def2d36d1fc",
+    "url": "/img/bg/10.jpg"
+  },
+  {
+    "revision": "d9401d206ec49e18976ebc767b48d525",
+    "url": "/img/bg/berry.gif"
+  },
+  {
+    "revision": "d0e4e2739380ab6cac3fbdfc886797a6",
+    "url": "/img/bg/bg1_h.jpg"
+  },
+  {
+    "revision": "f94c6b35cf9501823f69e66b1c01f3f1",
+    "url": "/img/bg/bg1_v.jpg"
+  },
+  {
+    "revision": "88efb60439d65fe3339f0285af9712ba",
+    "url": "/img/bg/bg1_v.png"
+  },
+  {
+    "revision": "8b588d6d4580f34c8f7a5d71e91ace0e",
+    "url": "/img/bg/bg2_h.jpg"
+  },
+  {
+    "revision": "a394c1b6a56c0079099c4ace6d673f91",
+    "url": "/img/bg/bg2_v.jpg"
+  },
+  {
+    "revision": "1be817fa0833e37c158e9a861c6355cd",
+    "url": "/img/bg/bg3_h.jpg"
+  },
+  {
+    "revision": "f4bc5930064b4e700440f7c35c3ada95",
+    "url": "/img/bg/bg3_v.jpg"
+  },
+  {
+    "revision": "4da99b005f2997c66646867a33e04d9f",
+    "url": "/img/bg/coffee1.gif"
+  },
+  {
+    "revision": "f4b54c484f5171a1655c78b26772884a",
+    "url": "/img/bg/coffee2.gif"
+  },
+  {
+    "revision": "58d93984c85313a6d9df5662197db487",
+    "url": "/img/bg/woman1.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "86659db2af78188345d3772f6c483a55",
+    "url": "/img/logo/coffee_trend1.png"
+  },
+  {
+    "revision": "f96d213e3ce81ead2931e310b86e834b",
+    "url": "/img/logo/kofeinya1.png"
+  },
+  {
+    "revision": "6a7bd8bee7d53e1a7b3994c5ba7d3751",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a071608911285d262c72",
+    "url": "/js/app.dde70bf1.js"
+  },
+  {
+    "revision": "d727346ef53102bee957",
+    "url": "/js/chunk-vendors.4d3f053c.js"
+  },
+  {
+    "revision": "e35ea6454653a10a52c4",
+    "url": "/js/footer.3179c7ba.js"
+  },
+  {
+    "revision": "0b1de269038a848e7995",
+    "url": "/js/footer~login.6a75cd3d.js"
+  },
+  {
+    "revision": "e434cc44b6c541eb0d64",
+    "url": "/js/login.05419975.js"
+  },
+  {
+    "revision": "3dbee3a3114bf15a7e70232703cf2dc5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "77430ba2e4162fb551709e5c4e9250c6",
+    "url": "/temp/breakfast.gif"
+  },
+  {
+    "revision": "0754e5332b2a5ffdba912cb7d15f3fd3",
+    "url": "/temp/breakfest1.gif"
+  },
+  {
+    "revision": "383bca8251c8a3513d51658ff2b32c81",
+    "url": "/temp/coffee.gif"
+  },
+  {
+    "revision": "4da99b005f2997c66646867a33e04d9f",
+    "url": "/temp/coffee1.gif"
+  },
+  {
+    "revision": "f4b54c484f5171a1655c78b26772884a",
+    "url": "/temp/coffee2.gif"
+  },
+  {
+    "revision": "4d33e933ae519c560658bd1afa7054f6",
+    "url": "/temp/coffee_h.gif"
+  },
+  {
+    "revision": "5a634b436bbea91747dda7e20bd5f378",
+    "url": "/temp/coffee_trend.png"
+  },
+  {
+    "revision": "86659db2af78188345d3772f6c483a55",
+    "url": "/temp/coffee_trend1.png"
+  },
+  {
+    "revision": "f42f4aed086871fd978aef977d21fe67",
+    "url": "/temp/drink1.gif"
+  },
+  {
+    "revision": "fa0b00f2fb3661cbb35920e488dc33d5",
+    "url": "/temp/elegant-coffee-shop-loyalty-card-template.zip"
+  },
+  {
+    "revision": "094b7f36c74e0b4f413751ee81fdd0f2",
+    "url": "/temp/honey.gif"
+  },
+  {
+    "revision": "d2f77f7047595e032606316b1d4e0861",
+    "url": "/temp/honey2.gif"
+  },
+  {
+    "revision": "dbb8843d8613ce12be8d32926ea69718",
+    "url": "/temp/icecream1.gif"
+  },
+  {
+    "revision": "f96d213e3ce81ead2931e310b86e834b",
+    "url": "/temp/kofeinya1.png"
+  },
+  {
+    "revision": "899a29fda198a6886d01f02c9138cca4",
+    "url": "/temp/lemon.gif"
+  },
+  {
+    "revision": "c1a2730bef9ed17e69e568c580d6b260",
+    "url": "/temp/modern-coffee-shop-loyalty-card-template.zip"
+  },
+  {
+    "revision": "51f18ce2722a336bc62f324d4359add0",
+    "url": "/temp/some.gif"
+  },
+  {
+    "revision": "5f9f0725e15abc891036ab782960bb97",
+    "url": "/temp/sugar.gif"
+  },
+  {
+    "revision": "2738e12d4180ac2866655a131f097898",
+    "url": "/temp/tea2.gif"
+  },
+  {
+    "revision": "735d17af2cb4d20f8b1dbfcf798a9815",
+    "url": "/temp/tea3.gif"
+  },
+  {
+    "revision": "dfa75a9b403e13665d8c6542e5ac2048",
+    "url": "/temp/tee.gif"
+  }
+]);
